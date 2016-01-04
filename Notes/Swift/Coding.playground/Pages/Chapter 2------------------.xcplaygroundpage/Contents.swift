bb0b42bf906af45017615b0a495cc28254826a0a
SACRELEE <sacrelee@gmail.com>
2016-01-04 22:04:19 +0800

    Declarations End

--- Notes/Swift/Coding.playground/Pages/Chapter 2------------------.xcplaygroundpage/Contents.swift
+++ Notes/Swift/Coding.playground/Pages/Chapter 2------------------.xcplaygroundpage/Contents.swift
@@ -4,3 +4,12 @@
 
 
 */
+
+import UIKit
+import XCPlayground
+
+var button = UIButton(type: UIButtonType.Custom)
+button.frame = CGRectMake(0, 0, 50, 20)
+button.setTitle("测试", forState: UIControlState.Normal)
+button.backgroundColor = UIColor.redColor()
+
