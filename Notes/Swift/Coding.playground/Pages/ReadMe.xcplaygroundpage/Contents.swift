28cc9c43c6caddce1b7b82305dcb458c9761e1e5
SACRELEE <sacrelee@gmail.com>
2016-01-07 10:52:02 +0800

    Blog1 End, Update ReadMe

--- Notes/Swift/Coding.playground/Pages/ReadMe.xcplaygroundpage/Contents.swift
+++ Notes/Swift/Coding.playground/Pages/ReadMe.xcplaygroundpage/Contents.swift
@@ -1,7 +1,12 @@
 /**
 *	@author SACRELEE, 2015-12-15 17:12:56
 *
+*  中文翻译项目地址：https://github.com/numbbbbb/the-swift-programming-language-in-chinese
+*
+*  感谢所有为翻译工作做出贡献的小伙伴们。
+*
 *  这里是跟随The Swift Programming language 的代码笔记，
+*
 *  起先是发现只是看书不敲出来，很容易忘记，所以开始用playground写这个笔记，要说有什么意义，看代码的效率应该比看书效率高吧。
 *
 *  为了自己理解，部分代码是自己写的例子，没有全部照搬教程。个人笔记，难免各种疏漏
